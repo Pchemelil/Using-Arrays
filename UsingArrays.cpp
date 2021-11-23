427dda8b4462b1a4c9b3b90ab2129076febf99f8
Pchemelil <85558913+Pchemelil@users.noreply.github.com>
2021-11-23 20:42:17 +0300

    Add files via upload

---
+++ UsingArrays.cpp
@@ -0,0 +1,11 @@
+//simple array declaration and initalization 
+#include <iostream>
+using namespace std;
+
+int main(){
+
+double nums[] = {12.25, 32.50, 16.90, 23, 45.68, 50.60};
+cout<< nums[0];
+return 0;
+
+}
